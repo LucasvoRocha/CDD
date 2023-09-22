e7f0a074ee9b767cc6cdd3872fe3d18dff170746
Lucas Rocha <144575286+LucasvoRocha@users.noreply.github.com>
2023-09-21 21:45:23 -0300

    Update For_Tabuada.py

--- For_Tabuada.py
+++ For_Tabuada.py
@@ -1,5 +1,5 @@
 numero_int = int(input("Digite um numero inteiro: "))
 
-for x in range(1, 10, 1):
+for x in range(1, 11, 1):
     tabuada = x * numero_int
     print(f"{x} x {numero_int} = {numero_int*x}")
