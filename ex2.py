7d73a366266b7093b38643b7c948aeda2597d81c
Lucas Rocha <144575286+LucasvoRocha@users.noreply.github.com>
2023-09-25 17:20:12 -0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,17 @@
+resp = "s"
+
+
+#================================================
+
+# while True:
+#     n1 = float(input("Digite um número: "))
+#     if n1 < 0:
+#         print("Este número é negativo.")
+#     elif n1 > 0:
+#         print("Este número é positivo.")
+#     else:
+#         input("Numero invalido. Digite outro número: ")
+#
+#     resp = input("Deseja fazer outra operação? S para sim, qualquer outra tecla para sair): ").lower()
+#     if resp != "s":
+#       break
