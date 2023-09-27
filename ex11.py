bff618566897550a00618121ddf0192d9bb76555
Lucas Rocha <144575286+LucasvoRocha@users.noreply.github.com>
2023-09-27 17:06:42 -0300

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,8 @@
+idade = int(input("Digite sua idade: "))
+mes = int(input("Digite os mes: "))
+dias = int(input("Digite os dias: "))
+anos = 365
+meses = 30
+dias2 = 1
+conta = (anos * idade + meses * mes + dias * dias)
+print(conta)
